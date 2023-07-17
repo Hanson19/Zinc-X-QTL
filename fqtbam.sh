30f0fcda91368d01ccfa1b6840d2b9611e592c66
Hanson19 <104232463+Hanson19@users.noreply.github.com>
2023-07-17 16:13:07 -0500

    Update fqtbam.sh

--- fqtbam.sh
+++ fqtbam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=zinc_bwa          # Name of job
 #SBATCH --mail-type=BEGIN,END,FAIL   # Set when I get emails (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=khanson15@ku.edu # Email address
+#SBATCH --mail-user=khanson15@ku.edu # Email address change 
 #SBATCH --partition=sjmac            # Either sixhour or sjmac
 #SBATCH --nodes=1                    # Number of nodes to run on
 #SBATCH --ntasks=1                   # Number of tasks to run (1 with >1 CPUS per task is multithreaded)
@@ -18,9 +18,9 @@ module load bcftools/1.9
 ref="ref/dm6.fa"
 dir1="process"
 dir2="data/bam/pbam"
-files="data/raw/readname.mapping.txt" #This is that 5 column list that made above. its basically the i in (I think)
+files="data/raw/readname.mapping.txt" #This is that 5 column list that made above.
 
-shortname=`head -n $SLURM_ARRAY_TASK_ID $files | tail -n 1 | cut -f1` #Shortname is column 1 of readname.mapping.txt the end of it is row column 
+shortname=`head -n $SLURM_ARRAY_TASK_ID $files | tail -n 1 | cut -f1` 
 F1=`head -n $SLURM_ARRAY_TASK_ID $files | tail -n 1 | cut -f2`
 F2=`head -n $SLURM_ARRAY_TASK_ID $files | tail -n 1 | cut -f3`
 R1=`head -n $SLURM_ARRAY_TASK_ID $files | tail -n 1 | cut -f4`
@@ -28,6 +28,6 @@ R2=`head -n $SLURM_ARRAY_TASK_ID $files | tail -n 1 | cut -f5`
 
 bwa mem -t 4 -M $ref $F1 $R1 | samtools view -bS - > $dir1/$shortname.1.temp.bam
 bwa mem -t 4 -M $ref $F2 $R2 | samtools view -bS - > $dir1/$shortname.2.temp.bam
-samtools merge -f $dir1/$shortname.temp.bam $dir1/$shortname.1.temp.bam $dir1/$shortname.2.temp.bam #merging bam files
+samtools merge -f $dir1/$shortname.temp.bam $dir1/$shortname.1.temp.bam $dir1/$shortname.2.temp.bam 
 samtools sort $dir1/$shortname.temp.bam -o $dir2/$shortname.bam
 samtools index $dir2/$shortname.bam
